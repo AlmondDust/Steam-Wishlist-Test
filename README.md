ff337eea6f9d020c75b3651d7666e0f6e6ffcc54
AlmondDust <adam.s.buchinsky@gmail.com>
2023-05-24 00:58:33 -0400

    sets up steam api access and flask debug

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Steam-Wishlist-Test
 Website to test if you (Lucas) have any idea what is on your own Steam wishlist
+
+## Setup
+
+### .env file
+In the main Steam-Wishlist_test directory create and env file with this contents, filling in the relevant information:
+```
+STEAM_API_KEY=<YOUR_STEAM_API KEY>
+```
--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
+
+from steam import Steam
+from decouple import config
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    config.encoding = 'cp1251'
+    KEY = config("STEAM_API_KEY")
+
+    print(f'Key = {KEY}')
+
+    steam = Steam(KEY)
+    user = steam.users.search_user("the12thchairman")
+
+    raise Exception('test exception')
+
+    return repr(user)
 
 
 if __name__ == "__main__":
